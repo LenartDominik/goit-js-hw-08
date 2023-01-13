0dfbabc54ea41f83667b68d746692336dea5b71d
Dominik Lenart <dominik.lenart84@gmail.com>
2023-01-13 16:24:09 +0100

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+const throttle = require('lodash.throttle');
 
+const localStorage = window.localStorage;
+const feedbackFormState = 'feedback-form-state';
+const form = document.querySelector('form');
+const emailInput = document.querySelector('input');
+const messageTextarea = document.querySelector('textarea');
+const formElements = Array.from(form.elements);
+
+const inputEvent = () => {
+  localStorage.setItem(
+    feedbackFormState,
+    JSON.stringify({
+      email: formElements[0].value,
+      message: formElements[1].value,
+    })
+  );
+};
+
+form.addEventListener('input', throttle(inputEvent, 500));
+
+const submitEvent = e => {
+  e.preventDefault();
+  console.log({
+    email: formElements[0].value,
+    message: formElements[1].value,
+  });
+  localStorage.removeItem(feedbackFormState);
+  form.reset();
+};
+
+form.addEventListener('submit', submitEvent);
+
+window.addEventListener('load', () => {
+  if (localStorage.getItem(feedbackFormState) !== null) {
+    const getStorageData = JSON.parse(localStorage.getItem(feedbackFormState));
+    emailInput.value = getStorageData.email;
+    messageTextarea.value = getStorageData.message;
+  }
+});
